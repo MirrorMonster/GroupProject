7bee5b4505b250592faf6e9c3f4c9c5f235b8ea7
Dam Thinh <thinhquangshin@gmail.com>
2020-05-18 08:50:26 +0700

    add data patern

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace GroupProject
 {
+
+    struct Loai
+    {
+        string ten;
+        int ma;
+        int tongSL;
+    }
+
+    struct HoaQua
+    {
+        int ma;
+        string ten;
+        int soLuong;
+        DateTime ngayNhap;
+        DateTime ngayHetHan;
+        string xuatSu;
+        int giaNhap;
+        int giaBan;
+        int khoiLuong;
+        bool nhapKhau;
+        string loai;
+    }
+
+
     class Program
     {
+        static List<HoaQua> listHoaQua;
+        static List<Loai> listLoai;
+
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
         }
     }
 }
