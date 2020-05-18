a9d998995dc132d18df5dcd1fc8f75274fc6385e
Hoang Quynh <hoangquynh12a3dqh@gmail.com>
2020-05-18 08:53:53 +0700

    test

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace GroupProject
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+        }
+        static void timkiem()
+        {
+            Console.WriteLine("Nhập loại sản phẩm muốn tìm:");
+            string loai = Console.ReadLine();
+            loai = loai.Trim().ToLower();
+            while (loai.Contains("  "))
+            {
+                loai = loai.Replace("  ", " ");
+            }    
+            
         }
     }
 }
