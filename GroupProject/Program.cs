c28ac41e974166952cc00995435a898a5e97139d
Dam Thinh <thinhquangshin@gmail.com>
2020-05-18 18:44:42 +0700

    just check code

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -7,26 +7,26 @@ using System.Threading.Tasks;
 namespace GroupProject
 {
 
-    struct Loai
+    public struct Loai
     {
         string ten;
         int ma;
         int tongSL;
     }
 
-    struct HoaQua
+    public struct HoaQua
     {
-        int ma;
-        string ten;
-        int soLuong;
-        DateTime ngayNhap;
-        DateTime ngayHetHan;
-        string xuatSu;
-        int giaNhap;
-        int giaBan;
-        int khoiLuong;
-        bool nhapKhau;
-        string loai;
+        public int ma;
+        public string ten;
+        public int soLuong;
+        public DateTime ngayNhap;
+        public DateTime ngayHetHan;
+        public string xuatSu;
+        public int giaNhap;
+        public int giaBan;
+        public int khoiLuong;
+        public bool nhapKhau;
+        public string loai;
     }
 
 
@@ -38,6 +38,60 @@ namespace GroupProject
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+            
+        }
+
+        public static void NhapHoaQua()
+        {
+            while (true)
+            {
+                Console.Write("mã sản phẩm: ");
+                int ma = int.Parse(Console.ReadLine());
+
+                Console.Write("tên sản phẩm: ");
+                string ten = Console.ReadLine();
+
+                Console.Write("ngày nhập: ");
+                DateTime ngayNhap = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("hạn sử dụng: ");
+                DateTime ngayHetHan = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("số lượng: ");
+                int soLuong = int.Parse(Console.ReadLine());
+
+                Console.Write("xuất xứ:");
+                string xuatXu = Console.ReadLine();
+
+                Console.Write("giá nhập: ");
+                int giaNhap = int.Parse(Console.ReadLine());
+
+                Console.Write("giá bán: ");
+                int giaBan = int.Parse(Console.ReadLine());
+
+                Console.Write("khối lượng mỗi sản phẩm: ");
+                int khoiLuong = int.Parse(Console.ReadLine());
+
+                Console.Write("là hàng nhập khẩu: ");
+                bool laNhapKhau = bool.Parse(Console.ReadLine());
+
+                Console.Write("Loại: ");
+                string loai = Console.ReadLine();
+
+                HoaQua hoaqua = new HoaQua();
+                hoaqua.ma = ma;
+                hoaqua.ten = ten;
+                hoaqua.ngayNhap = ngayNhap;
+                hoaqua.ngayHetHan = ngayHetHan;
+                hoaqua.soLuong = soLuong;
+            }
+        }
+
+        public static void ThongKe()
+        {
+            Console.WriteLine("Có {0} loại quả trong siêu thị." + listLoai.Count());
+            
+            
         }
     }
 }
