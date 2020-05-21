f72a73b26866f3c92e6dc38bee8d490cef4f72b7
Dam Thinh <thinhquangshin@gmail.com>
2020-05-21 09:29:35 +0700

    lay du lieu

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -265,6 +265,11 @@ namespace GroupProject
 
         }
 
+        public static void LayDuLieu()
+        {
+
+        } 
+
         static void TimKiem(int ma)
         {
             Console.WriteLine("Nhập loại sản phẩm muốn tìm:");
